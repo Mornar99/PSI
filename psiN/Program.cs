4131c5122b2c3feb283262ddee97b910f901e09d
Ivan <imorna00@fesb.hr>
2023-03-16 17:20:10 +0100

    commit test

--- psiN/Program.cs
+++ psiN/Program.cs
@@ -6,7 +6,7 @@ namespace psiN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wo");
         }
     }
 }
